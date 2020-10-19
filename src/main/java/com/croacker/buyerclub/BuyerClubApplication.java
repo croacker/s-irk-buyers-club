1992990ce2d10317fe3a375495cb848feef8d6d3
gumenyuk_av2 <q1234567yY>
2020-10-19 18:22:49 +0800

    init

---
+++ src/main/java/com/croacker/buyerclub/BuyerClubApplication.java
@@ -0,0 +1,13 @@
+package com.croacker.buyerclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyerClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/croacker/buyerclub/BuyerClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.croacker.buyerclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyerClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
