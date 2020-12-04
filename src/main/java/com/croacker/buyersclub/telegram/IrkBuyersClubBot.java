85cb7a841ed0fab3f074334420b82d3bd91e9573
gumenyuk_av2 <q1234567yY>
2020-12-05 00:50:40 +0800

    Добавить соединение с telegram #2

---
+++ src/main/java/com/croacker/buyersclub/telegram/IrkBuyersClubBot.java
@@ -0,0 +1,21 @@
+package com.croacker.buyersclub.telegram;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class IrkBuyersClubBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
