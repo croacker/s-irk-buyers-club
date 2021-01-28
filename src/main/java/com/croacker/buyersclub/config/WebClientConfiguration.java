9f9fa5aa6fa7f189f237dacdd024d846ab1c58ff
gumenyuk_av2 <q1234567yY>
2021-01-28 21:07:30 +0800

    Добавить unit-тесты #36

--- src/main/java/com/croacker/buyersclub/config/WebClientConfiguration.java
+++ src/main/java/com/croacker/buyersclub/config/WebClientConfiguration.java
@@ -7,9 +7,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfiguration {
+
+    private final static int MAX_IN_MEMORY_SIZE = 16777216;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder().exchangeStrategies(ExchangeStrategies.builder()
-                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16777216)).build()).build();
+                .codecs(configurer -> configurer
+                        .defaultCodecs()
+                        .maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                .build()).build();
     }
 }
